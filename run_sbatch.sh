a63fa19a422a13e05bbc4534a0f2749e24faa611
Frauke Beyer <fbeyer@cbs.mpg.de>
2020-04-21 17:07:53 +0200

    changed

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -1,1 +1,5 @@
 sbatch --array=1-$(( $( wc -l /ptmp/fbeyer/BIDS_test/participants.tsv | cut -f1 -d' ' ) - 1 )) sbatch.slurm
+
+
+
+singularity run -B /ptmp/fbeyer/BIDS_test,/ptmp/fbeyer/BIDS_test/derivatives,/ptmp/fbeyer/wd /u/fbeyer/mriqc_0.15.0.simg /ptmp/fbeyer/BIDS_test /ptmp/fbeyer/BIDS_test/derivatives/mriqc participant -m T1w -w /ptmp/fbeyer/wd
